79dcf22cc7888b2ca060f2c5a63ef8e003ff966a
Emeka <okorochukwuemekaanthony@gmail.com>
2025-07-15 11:09:39 +0100

    Updated app to generate recharge codes

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 const express = require('express');
 const app = express();
-app.get('/', (req, res) => res.send('Welcome to NodeJs simple service, Please deploy me Deploy me!'));
+const randomstring = require('randomstring');
+app.get('/', (req, res) => res.send('Welcome to NodeJs simple service, Please deploy me Deploy me! <br>    <p style="color: green;">Please manage this recharge card</p>  ' + randomstring.generate(16)));
 app.listen(3501, () => console.log(`Listening on port 3501`));
+
+function generateRandomPassword(length = 12) {
+    return  cryptoRandomString({ length: 16 });
+  }
